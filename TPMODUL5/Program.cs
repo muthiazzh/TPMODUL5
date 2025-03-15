8160becc26c4fdcfef03e10a7d512c9daee12484
muthiazzh <muthiahazzahra200@gmail.com>
2025-03-15 22:34:41 +0700

    menambahkan class DataGeneric

--- TPMODUL5/Program.cs
+++ TPMODUL5/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+public class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DataGeneric<string> nimData = new DataGeneric<string>("103022330117");
+
+        nimData.PrintData();
+    }
+}
